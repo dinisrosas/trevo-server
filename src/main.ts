725e514d9fc61069cfab44677e9cfda8d2ce8ae1
dinisrosas <dinisrosas@hotmail.com>
2021-11-27 18:34:19 +0000

    refactor: remove csurf

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
-import * as csurf from 'csurf';
+// import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-  app.use(csurf());
+  // app.use(csurf());
   app.enableCors();
 
   await app.listen(process.env.PORT || 8080);
