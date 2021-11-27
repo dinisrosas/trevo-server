3c5361de424942b227e18b04ad31270d488ae9ea
dinisrosas <dinisrosas@hotmail.com>
2021-11-27 17:44:18 +0000

    add: helmet, csurf and rate limiter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(csurf());
+
   await app.listen(process.env.PORT || 8080);
 }
 
