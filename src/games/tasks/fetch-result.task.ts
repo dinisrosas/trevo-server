7507c53f10ef5c99f2086c88a4657ac7cae5cb4e
dinisrosas <dinisrosas@hotmail.com>
2021-11-28 19:40:20 +0000

    fix: fetch bet result task

--- src/games/tasks/fetch-result.task.ts
+++ src/games/tasks/fetch-result.task.ts
@@ -1,25 +1,27 @@
-import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { GamesService } from '../games.service';
 import { getLatestGameResult } from '../helpers/result.helper';
 
 @Injectable()
 export class FetchGameResultTask {
+  private readonly logger = new Logger(FetchGameResultTask.name);
+
   constructor(private gamesService: GamesService) {}
 
   @Cron('0 22 * * 1-3,5,6')
   @Cron('30 14 * * 4')
   async fetchAndUpdateGameResult(): Promise<void> {
-    // get today's games
     const activeGames = await this.gamesService.findRecentActiveGames();
 
     for (const game of activeGames) {
       const { result, isoDate } = await getLatestGameResult(game.type);
 
       if (game.isoDate !== isoDate) {
-        throw new NotAcceptableException(
-          'The date of the game and the date of the result fetched do not match',
-        );
+        this.logger.warn('Game date and result date do not match');
+        this.logger.log(isoDate);
+        this.logger.log(JSON.stringify(game, null, 2));
+        continue;
       }
 
       await this.gamesService.updateResult(game.id, result);
