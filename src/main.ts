e85c56dd4a8d5879458cbbea57af5e321ae86766
dinisrosas <dinisrosas@hotmail.com>
2022-04-10 22:50:42 +0100

    fix: remove helmet config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
+// import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  app.use(helmet({ contentSecurityPolicy: false }));
+  // app.use(helmet({ contentSecurityPolicy: false }));
 
   await app.listen(process.env.PORT || 8080);
 }
